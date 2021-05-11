b257f9e1095484d5db431a6e1fdfe4fe88f60609
Spyros <orfanos@ualberta.ca>
2021-05-10 21:12:22 -0400

    Automated the extraction of decision rules from regression trees. Changed to ReLU activation.

--- Setup/02_regressionTree.py
+++ Setup/02_regressionTree.py
@@ -1,12 +1,7 @@
 import numpy as np
 from sklearn import tree
-
-import matplotlib.pyplot as plt
 import pandas as pd
 import re
-
-
-
 
 # Load trajectories
 trajs = pd.read_csv("trajectory.csv")
@@ -15,16 +10,17 @@ X = trajs[['o[0]', 'o[1]', 'o[2]', 'o[3]']]
 y1 = trajs['N1']
 y2 = trajs['N2']
 
-# Neuron 1
+# Regression tree for Neuron 1
 regr_1 = tree.DecisionTreeRegressor(max_depth=2, random_state=1)
 regr_1.fit(X, y1)
-
 tree_rules = tree.export_text(regr_1, feature_names=list(X.columns))
+print(tree_rules)
+
+# Extract decision rules as strings
 decision_rules = tree_rules.replace("|--- value:", "return")
 decision_rules = decision_rules.replace("|---", "if")
 decision_rules = decision_rules.replace("|", "")
-boolean_rules1 = np.unique(re.findall(r'if (.*)', decision_rules))
-print(tree_rules)
+boolean_rules = re.findall(r'if (.*)', decision_rules)
 
 """
 print(np.mean(trajs.query('o[1] <= -0.17 & o[0] <= -0.37 & o[3] <= 0.70')['a']))
@@ -40,19 +36,21 @@ print(np.mean(trajs.query('o[1] > -0.17 & o[0] > -0.33 & o[1] <= -0.04')['a']))
 print(np.mean(trajs.query('o[1] > -0.17 & o[0] > -0.33 & o[1] > -0.04')['a']))
 """
 
-# Neuron 2
+# Regression tree for Neuron 2
 regr_2 = tree.DecisionTreeRegressor(max_depth=3, random_state=1)
 regr_2.fit(X, y2)
-
 tree_rules = tree.export_text(regr_2, feature_names=list(X.columns))
+print(tree_rules)
+
+# Extract decision rules
 decision_rules = tree_rules.replace("|--- value:", "return")
 decision_rules = decision_rules.replace("|---", "if")
 decision_rules = decision_rules.replace("|", "")
-boolean_rules2 = np.unique(re.findall(r'if (.*)', decision_rules))
-print(tree_rules)
+boolean_rules.extend(re.findall(r'if (.*)', decision_rules))
+
+# Only keep unique boolean expressions
+boolean_rules = list(set(boolean_rules))
 
 print("Decision Rules:")
-print(boolean_rules2)
-print(type(boolean_rules2))
-
-print([boolean_rules1, boolean_rules2])
+print(boolean_rules)
+np.savetxt("boolean_rules.txt", boolean_rules, fmt='%s')
--- Setup/CartPole_IRL.R
+++ Setup/CartPole_IRL.R
@@ -9,31 +9,107 @@ toBinary = function(x, threshold){
   return(x)
 }
 
+##################
+## 1- LOAD DATA ##
+#################
 # Observations (state)
 # 0       Cart Position             -4.8                    4.8
 # 1       Cart Velocity             -Inf                    Inf
 # 2       Pole Angle                -0.418 rad (-24 deg)    0.418 rad (24 deg)
 # 3       Pole Angular Velocity     -Inf                    Inf
-obs <- read.table("~/Desktop/TLOS/School/Alberta/Fall_2020/CMPUT_653/Assignment 2/part2/obsO_0.txt", quote="\"")
+obs <- read.table("~/Documents/GitHub/MastersResearch_orfanos/Setup/obsO_0.txt", quote="\"")
 names(obs) = c("x", "v", "theta", "omega")
 
-# Neurons 
-P1 <- read.table("~/Desktop/TLOS/School/Alberta/Fall_2020/CMPUT_653/Assignment 2/part2/predP1_0.txt", quote="\"")
-P2 <- read.table("~/Desktop/TLOS/School/Alberta/Fall_2020/CMPUT_653/Assignment 2/part2/predP2_0.txt", quote="\"")
+# Actions
+actions <- read.table("~/Documents/GitHub/MastersResearch_orfanos/Setup/actions_0.txt", quote="\"")
+names(actions) = "a"
+
+# Neurons
+P1 <- read.table("~/Documents/GitHub/MastersResearch_orfanos/Setup/predP1_0.txt", quote="\"")
+P2 <- read.table("~/Documents/GitHub/MastersResearch_orfanos/Setup/predP2_0.txt", quote="\"")
 
 names(P1) = "P1"
 names(P2) = "P2"
 
-data = cbind(obs, P1, P2)
+data = cbind(obs, actions, P1, P2)
+write.csv(data, file="data.csv")
+getwd()
 
 
 
-# position and velocity 
+##################
+## 2- Plot DATA ##
+#################
+# position and velocity
 scatter3D(x=data$x, data$v, data$P1, phi = 10, bty = "g",  theta = 120, type = "h",  pch = 19, cex = 0.5, ticktype = "detailed", xlab = "pos", ylab = "vel", zlab = "Z")
 
-# angle and angular velocity 
+# angle and angular velocity
 scatter3D(x=data$theta, data$omega, data$P1, phi = 20, bty = "g",  theta = -45, type = "h",  pch = 19, cex = 0.5, ticktype = "detailed", xlab = "theta", ylab = "omega", zlab = "Z")
 scatter3D(x=data$theta, data$omega, data$P2, phi = 10, bty = "g",  theta = -45, type = "h",  pch = 19, cex = 0.5, ticktype = "detailed", xlab = "theta", ylab = "omega", zlab = "Z")
+
+plot(data$theta, data$omega, col= ifelse(data$P2 >= 0.5, "red", "blue"), ylim = c(0, 1))
+
+
+# tree
+# Regression Tree Example
+#install.packages("rpart")
+library(rpart)
+
+# NEURON 1
+fit <- rpart(P1~ x + v + theta + omega,
+             method="anova", data=data, maxdepth=3)
+
+rpart.rules(fit)
+
+
+printcp(fit) # display the results
+plotcp(fit) # visualize cross-validation results
+summary(fit) # detailed summary of splits
+
+# create additional plots
+par(mfrow=c(1,2)) # two plots on one page
+rsq.rpart(fit) # visualize cross-validation results
+
+# plot tree
+plot(fit, uniform=TRUE,
+     main="Regression Tree for Neuron 1")
+text(fit, use.n=TRUE, all=TRUE, cex=.8)
+
+
+mean(data[data$omega < 0.1643993 & data$x < 0.1943893, "a"])
+mean(data[data$omega < 0.1643993 & data$x >= 0.1943893, "a"])
+mean(data[data$omega >= 0.1643993 & data$x < 0.2381055, "a"])
+mean(data[data$omega >= 0.1643993 & data$x >= 0.2381055, "a"])
+
+
+# NEURON 2
+fit <- rpart(P2~ x + v + theta + omega,
+             method="anova", data=data, maxdepth=2)
+
+printcp(fit) # display the results
+plotcp(fit) # visualize cross-validation results
+summary(fit) # detailed summary of splits
+
+# create additional plots
+par(mfrow=c(1,2)) # two plots on one page
+rsq.rpart(fit) # visualize cross-validation results
+
+# plot tree
+plot(fit, uniform=TRUE,
+     main="Regression Tree for Neuron 2")
+text(fit, use.n=TRUE, all=TRUE, cex=.8)
+
+# candidates for actions
+mean(data[data$omega < -0.04620776 & data$theta < -0.008608433, "a"]) # 0.2 --> a=0
+mean(data[data$omega < -0.04620776 & data$theta >= -0.008608433, "a"]) # 0.34 --> a=0
+mean(data[data$omega >= -0.04620776 & data$theta < -0.0219456, "a"])  # 0.50 --> a= ???
+mean(data[data$omega >= -0.04620776 & data$theta >= -0.0219456, "a"]) # 0.72 --> a=1
+
+
+# create attractive postcript plot of tree
+#post(fit, file = "c:/tree2.ps",      title = "Regression Tree for Neuron 2")
+
+
 
 
 
@@ -43,13 +119,18 @@ binaryP1 = toBinary(P1, threshold)
 binaryP2 = toBinary(P2, threshold)
 data = cbind(obs, binaryP1, binaryP2)
 
-# position and velocity 
+# position and velocity
 scatter3D(x=data$x, data$v, data$P1, phi = 10, bty = "g",  theta = 120, type = "h",  pch = 19, cex = 0.5, ticktype = "detailed", xlab = "pos", ylab = "vel", zlab = "Z")
 
 
-# angle and angular velocity 
+# angle and angular velocity
 scatter3D(x=data$theta, data$omega, data$P1, phi = 30, bty = "g",  theta = -45, type = "h",  pch = 19, cex = 0.5, ticktype = "detailed", xlab = "theta", ylab = "omega", zlab = "Z", main="N1 Output")
 scatter3D(x=data$theta, data$omega, data$P2, phi = 30, bty = "g",  theta = -45, type = "h",  pch = 19, cex = 0.5, ticktype = "detailed", xlab = "theta", ylab = "omega", zlab = "Z", main="N1 Output")
+
+plot(data$theta, data$omega, col= ifelse(data$P2 == 1, "red", "blue"), xlab = "theta", ylab = "omega", pch=20)
+legend("bottomleft", title = "neuron value \n rounded", legend = c("0","1"), col = c("blue", "red"),  pch=19, cex=0.8)
+abline(v=-0.03, )
+abline(h=0, v=NULL)
 
 
 # def theta_omega_policy(obs):
@@ -59,4 +140,27 @@ scatter3D(x=data$theta, data$omega, data$P2, phi = 30, bty = "g",  theta = -45, 
 # else:
 #   return 0 if theta < 0 else 1
 
-#if -0.03 < theta & theta < 0.03 
+#if abs(theta) < 0.03 & w < 0
+# return 0
+#if abs(theta) < 0.03 & w >= 0
+# return 1
+#if abs(theta) > 0.03 & w < 0
+# return 0
+#if abs(theta) > 0.03 & theta < 0 (i.e if theta < -0.03)
+# return 0
+
+
+xpoints = 1:20
+y = rnorm(20)
+plot(NULL,ylim=c(-3,3),xlim=c(1,20))
+abline(v=xpoints,col="gray90",lwd=80)
+abline(v=xpoints,col="white")
+abline(h = 0, lty = 2)
+points(xpoints, y, pch = 16, cex = 1.2, col = "red")
+
+
+mean(data[data$omega < -0.04620776 & data$theta < -0.008608433, "a"]) # 0.2 --> a=0
+mean(data[data$omega < -0.04620776 & data$theta >= -0.008608433, "a"]) # 0.34 --> a=0
+mean(data[data$omega >= -0.04620776 & data$theta < -0.0219456, "a"])  # 0.50 --> a= ???
+mean(data[data$omega >= -0.04620776 & data$theta >= -0.0219456, "a"]) # 0.72 --> a=1
+
